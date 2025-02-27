7322b7842733a60949ae770967cae6b210067fd7
Aman Puri <aman.puri@menlosecurity.com>
2025-02-27 16:13:27 -0300

    First Build Test

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello from Argo CD & Cloud Deploy!');
+  res.send('Hello from Argo CD & Cloud Deploy and PSO!');
 });
 
 app.listen(port, () => {
